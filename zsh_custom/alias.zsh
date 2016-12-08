5fa3407e89a34f5f4d27a07f6a3422f87fe97103
xashes <xashes@outlook.com>
2016-12-08 15:45:10 +0800

    path and alias

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -1,23 +1,23 @@
 # Python tools - added at 2015-02-10
-alias ipqt='ipython qtconsole&'
-alias junote='jupyter notebook&'
+alias jpqt='jupyter qtconsole&'
+alias jpnote='jupyter notebook&'
 # Emacs
 alias ed='emacs --daemon'
 alias et='emacsclient -t'
 alias ec='emacsclient -c &'
 
 # Thefuck
-alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
-alias FUCK='fuck'
+# alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
+# alias FUCK='fuck'
 
 
 # Linux only
 # ----------------------------------------------------------
 # Custom keymap
-alias xmod='xmodmap ~/.Xmodmap'
+# alias xmod='xmodmap ~/.Xmodmap'
 
 # sudo using user $PATH
-alias sudoa='sudo env PATH=$PATH'
+# alias sudoa='sudo env PATH=$PATH'
 
 # xMind
 # alias xmind='cd ~/Applications/xMind/XMind_Linux_64bit/ && ./XMind'
---
+++ zsh_custom/config.zsh
@@ -0,0 +1,40 @@
+# User configuration
+
+# export PATH=$HOME/bin:$PATH
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# add Anaconda3 2.1.0 path by xashes (2015-02-08)
+export PATH="/Users/xashes/anaconda3/bin:$PATH"
+
+# add racket path by xashes
+# export PATH="/Applications/Racket v6.1.1/bin:$PATH"
+
+# add postgres path by xashes at (2015-05-08)
+# export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
+
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"

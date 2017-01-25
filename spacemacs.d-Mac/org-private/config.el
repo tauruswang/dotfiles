71a40b99dd070dd48a53478c1011b7d1e2441106
xashes <xashes@outlook.com>
2017-01-25 09:34:52 +0800

    add org-capture-templates

--- spacemacs.d-Mac/org-private/config.el
+++ spacemacs.d-Mac/org-private/config.el
@@ -7,17 +7,121 @@
   (setq org-default-notes-file "~/org/organizer.org")
 
   ;; Capture templates
+  (defvar my/org-basic-task-template "* TODO %^{Task}
+:PROPERTIES:
+:Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|4:00}
+:END:
+Captured %<%Y-%m-%d %H:%M>
+%?
+
+%i
+" "Basic task data")
+
   (setq org-capture-templates
-        '(("a" "Account" entry (file "~/org/login.org")
-           "* %?\nentered on %U\n  %i\n  ")
-          ("j" "Journal" entry (file+datetree "~/org/journal.org")
-           "* %^{Brief Description} %^g\n%?\nadded: %U")
-          ("n" "Note" entry (file "~/org/note.org")
-           "* %^{Brief Description} %^g\n%?\nadded: %U")
-          ("t" "Todo" entry
-           (file+headline "~/org/todo.org" "Todo")
-           "* inbox %^{Brief Description} %^g\n%?\nadded: %U")
-          ))
+        `(("t" "Tasks" entry
+           (file+headline "~/org/organizer.org" "Inbox")
+           ,my/org-basic-task-template)
+          ("T" "Quick task" entry
+           (file+headline "~/org/organizer.org" "Inbox")
+           "* TODO %^{Task}\nSCHEDULED: %t\n"
+           :immediate-finish t)
+          ("i" "Interrupting task" entry
+           (file+headline "~/org/organizer.org" "Inbox")
+           "* STARTED %^{Task}"
+           :clock-in :clock-resume)
+          ("e" "Emacs idea" entry
+           (file+headline "~/org/emacs-notes/tasks.org" "Emacs")
+           "* TODO %^{Task}"
+           :immediate-finish t)
+          ("E" "Energy" table-line
+           (file+headline "~/org/organizer.org" "Track energy")
+           "| %U | %^{Energy 5-awesome 3-fuzzy 1-zzz} | %^{Note} |"
+           :immediate-finish t
+           )
+          ("b" "Business task" entry
+           (file+headline "~/org/business.org" "Tasks")
+           ,my/org-basic-task-template)
+          ("p" "People task" entry
+           (file+headline "~/org/people.org" "Tasks")
+           ,my/org-basic-task-template)
+          ("j" "Journal entry" plain
+           (file+datetree "~/org/journal.org")
+           "%K - %a\n%i\n%?\n"
+           :unnarrowed t)
+          ("J" "Journal entry with date" plain
+           (file+datetree+prompt "~/org/journal.org")
+           "%K - %a\n%i\n%?\n"
+           :unnarrowed t)
+          ("s" "Journal entry with date, scheduled" entry
+           (file+datetree+prompt "~/org/journal.org")
+           "* \n%K - %a\n%t\t%i\n%?\n"
+           :unnarrowed t)
+          ("c" "Protocol Link" entry (file+headline ,org-default-notes-file "Inbox")
+           "* [[%:link][%:description]] \n\n#+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n%?\n\nCaptured: %U")
+          ("db" "Done - Business" entry
+           (file+headline "~/org/business.org" "Tasks")
+           "* DONE %^{Task}\nSCHEDULED: %^t\n%?")
+          ("dp" "Done - People" entry
+           (file+headline "~/org/people.org" "Tasks")
+           "* DONE %^{Task}\nSCHEDULED: %^t\n%?")
+          ("dt" "Done - Task" entry
+           (file+headline "~/org/organizer.org" "Inbox")
+           "* DONE %^{Task}\nSCHEDULED: %^t\n%?")
+          ("q" "Quick note" item
+           (file+headline "~/org/organizer.org" "Quick notes"))
+          ("l" "Ledger entries")
+          ("lm" "MBNA" plain
+           (file "~/org/ledger")
+           "%(org-read-date) %^{Payee}
+    Liabilities:MBNA
+    Expenses:%^{Account}  $%^{Amount}
+  " :immediate-finish t)
+          ("ln" "No Frills" plain
+           (file "~/org/ledger")
+           "%(let ((org-read-date-prefer-future nil)) (org-read-date)) * No Frills
+    Liabilities:MBNA
+    Assets:Wayne:Groceries  $%^{Amount}
+  " :immediate-finish t)
+          ("lc" "Cash" plain
+           (file "~/org/ledger")
+           "%(org-read-date) * %^{Payee}
+    Expenses:Cash
+    Expenses:%^{Account}  %^{Amount}
+  ")
+          ("B" "Book" entry
+           (file+datetree "~/org/books.org" "Inbox")
+           "* %^{Title}  %^g
+  %i
+  *Author(s):* %^{Author} \\\\
+  *ISBN:* %^{ISBN}
+
+  %?
+
+  *Review on:* %^t \\
+  %a
+  %U"
+           :clock-in :clock-resume)
+           ("C" "Contact" entry (file "~/org/contacts.org")
+            "* %(org-contacts-template-name)
+  :PROPERTIES:
+  :EMAIL: %(my/org-contacts-template-email)
+  :END:")
+           ("n" "Daily note" table-line (file+olp "~/org/organizer.org" "Inbox")
+            "| %u | %^{Note} |"
+            :immediate-finish t)
+           ("r" "Notes" entry
+            (file+datetree "~/org/organizer.org")
+            "* %?\n\n%i\n%U\n"
+            )))
+
+  (defun my/org-refile-and-jump ()
+    (interactive)
+    (if (derived-mode-p 'org-capture-mode)
+        (org-capture-refile)
+      (call-interactively 'org-refile))
+    (org-refile-goto-last-stored))
+  (eval-after-load 'org-capture
+    '(bind-key "C-c C-r" 'my/org-refile-and-jump org-capture-mode-map))
 
   ;; Org-mode相关设置
   ;;(add-to-list 'auto-mode-alist '("\.\(org\|org_archive\|txt\)$" . org-mode))

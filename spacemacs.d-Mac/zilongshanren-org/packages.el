339044b7ccd1cb3feb5b0b2d4e90a31beb1d9bde
xashes <xashes@outlook.com>
2017-01-31 12:01:04 +0800

    use sacha's templates and change file path

--- spacemacs.d-Mac/zilongshanren-org/packages.el
+++ spacemacs.d-Mac/zilongshanren-org/packages.el
@@ -109,32 +109,100 @@
       ;; the %i would copy the selected text into the template
       ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
       ;;add multi-file journal
-      (setq org-capture-templates
-            '(("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
-               "* TODO [#B] %?\n  %i\n"
-               :empty-lines 1)
-              ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
-               "* %?\n  %i\n %U"
-               :empty-lines 1)
-              ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
-               "* TODO [#B] %?\n  %i\n %U"
-               :empty-lines 1)
-              ("s" "Code Snippet" entry
-               (file org-agenda-file-code-snippet)
-               "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
-              ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
-               "* TODO [#A] %?\n  %i\n %U"
-               :empty-lines 1)
-              ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
-               "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
-               :empty-lines 1)
-              ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
-               "* TODO [#C] %?\n  %i\n %a \n %U"
-               :empty-lines 1)
-              ("j" "Journal Entry"
-               entry (file+datetree org-agenda-file-journal)
-               "* %?"
-               :empty-lines 1)))
+  (defun my/org-contacts-template-email (&optional return-value)
+    "Try to return the contact email for a template.
+  If not found return RETURN-VALUE or something that would ask the user."
+    (or (cadr (if (gnus-alive-p)
+                  (gnus-with-article-headers
+                    (mail-extract-address-components
+                     (or (mail-fetch-field "Reply-To") (mail-fetch-field "From") "")))))
+        return-value
+        (concat "%^{" org-contacts-email-property "}p")))
+
+
+  (defvar my/org-basic-task-template "* TODO %^{Task}
+:PROPERTIES:
+:Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|4:00}
+:END:
+Captured %<%Y-%m-%d %H:%M>
+%?
+
+%i
+" "Basic task data")
+  (setq org-capture-templates
+        `(("t" "Tasks" entry
+           (file+headline org-agenda-file-gtd "Inbox")
+           ,my/org-basic-task-template)
+          ("T" "Quick task" entry
+           (file+headline org-agenda-file-gtd "Inbox")
+           "* TODO %^{Task}\nSCHEDULED: %t\n"
+           :immediate-finish t)
+          ("i" "Interrupting task" entry
+           (file+headline org-agenda-file-gtd "Inbox")
+           "* STARTED %^{Task}"
+           :clock-in :clock-resume)
+          ("j" "Journal entry" plain
+           (file+datetree org-agenda-file-journal)
+           "%K - %a\n%i\n%?\n"
+           :unnarrowed t)
+          ("J" "Journal entry with date" plain
+           (file+datetree+prompt org-agenda-file-journal)
+           "%K - %a\n%i\n%?\n"
+           :unnarrowed t)
+          ("s" "Journal entry with date, scheduled" entry
+           (file+datetree+prompt org-agenda-file-journal)
+           "* \n%K - %a\n%t\t%i\n%?\n"
+           :unnarrowed t)
+          ("q" "Quick note" item
+           (file+headline org-agenda-file-note "Quick notes"))
+          ("B" "Book" entry
+           (file+datetree "~/org/books.org" "Inbox")
+           "* %^{Title}  %^g
+  %i
+  *Author(s):* %^{Author} \\\\
+  *ISBN:* %^{ISBN}
+
+  %?
+
+  *Review on:* %^t \\
+  %a
+  %U"
+           :clock-in :clock-resume)
+           ("C" "Contact" entry (file "~/org/contacts.org")
+            "* %(org-contacts-template-name)
+  :PROPERTIES:
+  :EMAIL: %(my/org-contacts-template-email)
+  :END:")
+           ("n" "Daily note" table-line (file+olp org-agenda-file-gtd "Inbox")
+            "| %u | %^{Note} |"
+            :immediate-finish t)
+            ))
+;;      (setq org-capture-templates
+;;            '(("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
+;;               "* TODO [#B] %?\n  %i\n"
+;;               :empty-lines 1)
+;;              ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
+;;               "* %?\n  %i\n %U"
+;;               :empty-lines 1)
+;;              ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
+;;               "* TODO [#B] %?\n  %i\n %U"
+;;               :empty-lines 1)
+;;              ("s" "Code Snippet" entry
+;;               (file org-agenda-file-code-snippet)
+;;               "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
+;;              ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
+;;               "* TODO [#A] %?\n  %i\n %U"
+;;               :empty-lines 1)
+;;              ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
+;;               "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+;;               :empty-lines 1)
+;;              ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
+;;               "* TODO [#C] %?\n  %i\n %a \n %U"
+;;               :empty-lines 1)
+;;              ("j" "Journal Entry"
+;;               entry (file+datetree org-agenda-file-journal)
+;;               "* %?"
+;;               :empty-lines 1)))
 
       ;;An entry without a cookie is treated just like priority ' B '.
       ;;So when create new task, they are default

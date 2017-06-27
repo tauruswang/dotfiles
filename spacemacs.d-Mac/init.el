113446435763acba6766697a6ef3618896fec539
xashes <kellerx0319@gmail.com>
2017-06-27 20:58:19 +0800

    change theme list

--- spacemacs.d-Mac/init.el
+++ spacemacs.d-Mac/init.el
@@ -119,11 +119,8 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
+                         underwater
+                         sanityinc-tomorrow-blue
                          zenburn)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t

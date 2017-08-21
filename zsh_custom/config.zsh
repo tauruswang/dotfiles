b53f309c3f20475464bf7af75486d9b835dbae26
xashes <xashes@outlook.com>
2017-08-21 17:21:38 +0800

    add path for icu4c

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -5,6 +5,8 @@
 
 # add Anaconda3 2.1.0 path by xashes (2015-02-08)
 export PATH="$HOME/anaconda3/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/sbin:$PATH"
 
 # add racket path by xashes
 # export PATH="/Applications/Racket v6.1.1/bin:$PATH"

597fbc35524e8804140cebf2e8ead06efb29edf8
kellerx <keller0319@gmail.com>
2018-03-14 17:52:12 +0800

    set monospaced font to Ubuntu Mono for org-table

--- spacemacs.d-Mac/init.el
+++ spacemacs.d-Mac/init.el
@@ -357,7 +357,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 ;;  (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
-  (spacemacs//set-monospaced-font   "Source Code Pro" "Hiragino Sans GB" 18 20)
+  (spacemacs//set-monospaced-font   "Ubuntu Mono" "Ubuntu Mono" 18 18)
   (setenv "WORKON_HOME" "~/anaconda3/envs")
   (setq web-mode-engines-alist
         '(("django" . "\\.html\\'")))

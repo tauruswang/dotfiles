8432e49d5af590b2bb345a0750fef1295a300f9a
kellerx <keller0319@gmail.com>
2018-03-14 18:18:48 +0800

    change font to Ubuntu Mono

--- spacemacs.d-Mac/init.el
+++ spacemacs.d-Mac/init.el
@@ -171,8 +171,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 18
+   dotspacemacs-default-font '("Ubuntu Mono"
+                               :size 20
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
@@ -357,7 +357,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 ;;  (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
-  (spacemacs//set-monospaced-font   "Ubuntu Mono" "Ubuntu Mono" 18 18)
+;;  (spacemacs//set-monospaced-font   "Ubuntu Mono" "Ubuntu Mono" 18 18)
   (setenv "WORKON_HOME" "~/anaconda3/envs")
   (setq web-mode-engines-alist
         '(("django" . "\\.html\\'")))

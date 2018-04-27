e2c9421b88ec18b31194b456a04db0499c825250
kellerx <keller0319@gmail.com>
2018-04-28 01:06:58 +0800

    change font from 18 to 16

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -229,7 +229,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Space Mono"
-                               :size 18
+                               :size 16
                                :weight normal
                                :width normal)
 
@@ -496,7 +496,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 ;;  (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
-  (spacemacs//set-monospaced-font   "Space Mono" "WenQuanYi Micro Hei" 18 22)
+  (spacemacs//set-monospaced-font   "Space Mono" "WenQuanYi Micro Hei" 16 20)
   (setenv "WORKON_HOME" "~/anaconda3/envs")
   (setq web-mode-engines-alist
         '(("django" . "\\.html\\'")))

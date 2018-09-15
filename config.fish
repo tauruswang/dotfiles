4e4ac28ee1cdcf295496707ca41ea2ca3671046c
Keller <kellerx0319@gmail.com>
2018-09-15 11:53:06 +0800

    fish config file

---
+++ config.fish
@@ -0,0 +1,6 @@
+set PATH ~/anaconda3/bin $PATH
+
+thefuck --alias | source
+
+if test -f /home/xashes/.autojump/share/autojump/autojump.fish; . /home/xashes/.autojump/share/autojump/autojump.fish; end
+

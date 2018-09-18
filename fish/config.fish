b11cf5cd7cc57b87514a2334214b29bebb4b28a0
xashes <xashes@outlook.com>
2018-09-18 11:55:26 +0800

    add fish folder to dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
+set -x LANG en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 set PATH ~/anaconda3/bin $PATH
 
 thefuck --alias | source
 
 if test -f $HOME/.autojump/share/autojump/autojump.fish; . $HOME/.autojump/share/autojump/autojump.fish; end
 
-eval (pipenv --completion)
